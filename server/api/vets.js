d8b88fe9e92a1b3eeb43a1494c7cd87d0ba6b280
Nabil Yafai <nabilyafai90@gmail.com>
2018-10-15 15:25:24 -0400

    created get route for all vets

--- server/api/vets.js
+++ server/api/vets.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
 const { Vet } = require('../db/models');
 module.exports = router;
+
+router.get('/', async (req, res, next) => {
+  try {
+    const allVets = await Vet.findAll();
+    res.send(allVets);
+  } catch (err) {
+    next(err);
+  }
+});
