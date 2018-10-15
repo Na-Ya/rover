e4fd3c442881d9bfe0996bd8353c6552f8e7de76
Nabil Yafai <nabilyafai90@gmail.com>
2018-10-15 16:50:22 -0400

    added placeholder data in single vet component state to avoid throwing an error

--- client/components/singleVet.js
+++ client/components/singleVet.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import history from '../history';
 
 class SingleVet extends Component {
   constructor(props) {
     super(props);
-    this.state = { vetProfile: {} };
+    this.state = {
+      vetProfile: {
+        id: 0,
+        imgUrl:
+          'https://st2.depositphotos.com/4676065/10102/v/450/depositphotos_101023168-stock-illustration-loading-bar-icon.jpg',
+        name: '',
+        specialty: [],
+      },
+    };
   }
   componentDidMount() {
     const currentVetId = Number(this.props.match.params.id);
@@ -14,6 +23,10 @@ class SingleVet extends Component {
     this.setState({
       vetProfile: currentVet[0],
     });
+    this.nextVet = this.nextVet.bind(this);
+  }
+  nextVet() {
+    history.push(`/vets/${this.state.vetProfile.id + 1}`);
   }
   render() {
     const { id, imgUrl, name, specialty } = this.state.vetProfile;
@@ -22,7 +35,10 @@ class SingleVet extends Component {
         <img src={imgUrl} />
         <p>Name: {name}</p>
         <p>Specialties: {specialty.join(' , ')}</p>
-        <Link to={`/vets/${id + 1}`}> Next Vet </Link>
+        <button type="button" onClick={this.nextVet}>
+          {' '}
+          Next Vet
+        </button>
       </div>
     ) : (
       'Loading'
