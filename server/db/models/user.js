87cdb86acc9c5d8c38d1285989edff5aa9d42864
Nabil Yafai <nabilyafai90@gmail.com>
2018-10-15 15:00:45 -0400

    added pet name field to user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -8,6 +8,9 @@ const User = db.define('user', {
     unique: true,
     allowNull: false,
   },
+  petName: {
+    type: Sequelize.STRING,
+  },
   password: {
     type: Sequelize.STRING,
     // Making `.password` act like a func hides it when serializing to JSON.
