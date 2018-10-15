16cd62e80a7ad33d6f7457ba919b70cf9e964271
Nabil Yafai <nabilyafai90@gmail.com>
2018-10-15 16:03:37 -0400

    mapping through vets for all vet view, rendering their image, specialties, and name

--- client/components/allVets.js
+++ client/components/allVets.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-class AllVets extends Component {}
+class AllVets extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return this.props.vets.map(singleVet => {
+      return (
+        <div key={singleVet.id}>
+          <img src={singleVet.imgUrl} />
+          <p>Name: {singleVet.name}</p>
+          <p>Specialties: {singleVet.specialty.join(' , ')}</p>
+        </div>
+      );
+    });
+  }
+}
 
 const mapState = state => {
   return {
